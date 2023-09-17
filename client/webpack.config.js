5a7ba991fc2b0afaff599a9dab4404b7e652a38f
Joel Getzke <joelcode2023@gmail.com>
2023-09-17 17:41:35 -0500

    pwa manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,9 +33,9 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('assets/images/logo.png'),
+            src: path.resolve('favicon.ico'),
             sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-            destination: path.join('assets', 'icons')
+            destination: path.join('client', 'icons')
           }
         ]
       })
@@ -45,6 +45,21 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
